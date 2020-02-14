543f59e0da3d8dfb75ddbe749ff988c8d5d6c008
anamsoomro <anamsoomroed@gmail.com>
2020-02-13 18:40:45 -0600

    stay tweaking

--- app/run.rb
+++ app/run.rb
@@ -4,49 +4,48 @@ $prompt = TTY::Prompt.new
 $font = TTY::Font.new(:starwars)
 $pastel = Pastel.new
 
-# def exit_on_esc
-#     $prompt.on(:keypress) do |event|
-#         if event.value == 'esc'
-#             $prompt.trigger(exit)
-#         end
-#     end
-# end
-
-
 def user_login
-    # exit_on_esc
-    user_list = User.all.map {|user| user.username}
-    user = $prompt.select($pastel.yellow("Select Username"), ["new_user", user_list.sort_by{|user| user.downcase}].flatten, filter: true)
+    user = $prompt.select($pastel.yellow("Select Username"), ["new_user", users_alpha_order].flatten, filter: true)
     if user == "new_user"
-        new_user = $prompt.ask('enter username') do |username|
+        user = $prompt.ask('enter username') do |username|
             username.required true
-            username.validate {|name| User.usernames.include? name}
-            username.validate /\w/
+            username.messages[:required?] = 'Enter a username to continue...                      '
+            username.validate {|input| User.usernames.exclude? input}
+            username.messages[:valid?] = 'Username taken, please try again...                   '
         end
-        user = User.create(username: new_user).username
     end
-    @current_user = User.find_by(username: user)
+    @current_user = User.find_or_create_by(username: user)
+end
+
+def users_alpha_order
+    user_list = User.pluck(:username)
+    user_list.sort_by{|user| user.downcase}
 end
 
 def action
     $prompt.select($pastel.yellow("Watchu Wanna Do?"), filter: true) do |action|
-        action.choice 'start new game', -> {new_game}
+        action.choice 'start new game', -> {select_theme}
         action.choice 'checkout stats', -> {see_stats}
         action.choice 'exit', -> {exit_game}
     end
 end
 
 def select_theme
-    theme = $prompt.select("pick a theme", Theme.all.map{|theme| theme.name}, filter: true)
-    @current_theme = Theme.find_by(name: theme)
+    # theme = $prompt.select($pastel.yellow("pick a theme"), [$pastel.blue("Go Back"), Theme.all.map{|theme| theme.name}].flatten, filter: true)
+    theme = $prompt.select($pastel.yellow("pick a theme"), [$pastel.blue("Go Back"), Theme.pluck(:name)].flatten, filter: true)
+    if theme == "\e[34mGo Back\e[0m"
+        action
+    else
+        @current_theme = Theme.find_by(name: theme)
+        new_game
+    end
 end
 
 def new_game
     error = $pastel.red.bold.detach
-    words = select_theme.words.split(", ")
-    game_time = 30
+    words = @current_theme.words.split(", ")
+    game_time = 5
     typed_words = 0
-    # typed_words = []
     now = Time.now
     loop do
         if Time.now < now + game_time
@@ -60,7 +59,7 @@ def new_game
                     puts $pastel.yellow("TIMES UP!!")
                     break 
                 else 
-                    puts error.('Error!')
+                    puts error.(incorrect)
                     break 
                 end
             end 
@@ -68,7 +67,6 @@ def new_game
             break
         end
     end
-    # score = (typed_words.join(" ").split(" ").count.to_f / game_time.to_i * 60).to_i
     score = (typed_words.to_f / game_time.to_i * 60).to_i
     Game.create(score: score, user_id: @current_user.id, theme_id: @current_theme.id)
     print $pastel.yellow("YOUR SPEED: ") 
@@ -77,9 +75,13 @@ def new_game
     game_next
 end
 
+def incorrect
+    "Slow down!, Try again!, Try again bud!, Whoops!, Error!, Slow down bud!, Use the force next time!, You type like a strom trooper shoots!, Wrong!, ".split(", ").sample
+end
+
 def game_next
     $prompt.select("watchu wanna do next?", filter: true) do |action|
-        action.choice 'play again', -> {new_game}
+        action.choice 'play again', -> {select_theme}
         action.choice 'check stats', -> {see_stats}
         action.choice 'exit', -> {exit_game}
     end
@@ -101,7 +103,7 @@ end
 def stat_next
     $prompt.select($pastel.yellow("Watchu Wanna Do Next?"), filter: true) do |action|
         action.choice 'go back', -> {see_stats}
-        action.choice 'play game', -> {new_game}
+        action.choice 'play game', -> {select_theme}
         action.choice 'exit', -> {exit_game}
     end
 end
@@ -109,7 +111,7 @@ end
 def my_high_score
     if @current_user.games == []
         puts $pastel.blue("looks like you havent played a game, play!")
-        new_game
+        select_theme
     else
         @current_user.print_high_score
     end
@@ -118,7 +120,7 @@ end
 def my_global_rank
     if @current_user.games == []
         puts $pastel.blue("looks like you havent played a game, play you peasant!")
-        new_game
+        select_theme
     else
         @current_user.global_rank
     end
@@ -138,10 +140,8 @@ def play
     puts $pastel.yellow($font.write("                                           TYPE"))
     puts $prompt.say("                                                          YO DAWG, WELCOME!")
     puts $pastel.yellow($font.write("                                       WARS"))
-    # exit_on_esc
     user_login
     action
-    # pid = fork{ exec ‘killall’, afplay }
 end
 
 play
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,7 @@ u2 = User.create(username: "dumdum")
 u3 = User.create(username: "MuchFast_VeryType")
 u4 = User.create(username: "FastButWrong")
 u5 = User.create(username: "RightButSlow")
+u5 = User.create(username: "raul")
 
 # t1 = Theme.create(name: "the office", words: "Bears Beets Battlestar Galactica, Agent Michael Scarn, I am a little stitious, God is in this Chili's., Jim died of boredom., Eyes are the groin of the face., Did I stutter?, Do I need to be liked?, Who is Justice Beaver?, Sorry I annoyed you with my friendship., Dwight! You ignornat slut!, That's what she said." )
 # t2 = Theme.create(name: "coding", words: "ActiveRecord::Migration, def initialize, git pull origin master, attr_accessor, attr_reader, attr_writer, git commit -m 'I am fast', has_many :typos, belongs_to :winner, ActiveRecord::Base, \#{speedy}, Fruit is not a grape.")
@@ -16,13 +17,13 @@ u5 = User.create(username: "RightButSlow")
 # t6 = Theme.create(name: "Russian", words: "Dobraye ootro, Spaseeba preekrasna!, Bal'shoye spaseeba, Neechevo srashnava, Vi gavareetye, Kak pazhivayesh?, Ya plokha gavaryoo, Prasteete, Neeploha!")
 # t7 = Theme.create(name: "Star Wars", words: "force, Jedi Master, Sithe Lord, lightsaber, Naboo, X-win, Death Star, May the force be with you")
 
-t1 = Theme.create(name: "the office", words: "Bears, Beets, Chili, Jim, Dwight, Angela, Meredith, Stanley, Paper, Beesly, Halpert, Kevin, Andy, Creed, Kelly, Holly, Creed, Sales, Sabre, Pyramid, Scranton, Jello, Astrid")
-t2 = Theme.create(name: "coding", words: "ActiveRecord, initialize, origin, attr_accessor, attr_reader, attr_writer, has_many, belongs_to, \#{speedy}, GitHub, C#, JavaScript, Ruby, mongoDB, SQLite3, binding.pry, Sinatra, React, Redux, NodeJS, AngularJS, end, elsif")
-t3 = Theme.create(name: "numbers", words: "34297, 0000000, 2134, 0793, 13946, 387, 12345, 60038, 110981, 20163, 73756, 657, 09821, 7490, 74940, 8754, 86459, 7599, 27037, 47475, 549327, 459245, 47507, 4711118, 84027, 76703") #Array.new(20){rand(1..100000)}
-t4 = Theme.create(name: "runtime terror", words: "Blake, Caleb, Cornelius, Georgii, Gian, Placido, Raul, Jeannie, Somaia, Stephen, Steven, Vidhi, Mitchell, Paul, Vien")
-t5 = Theme.create(name: "jibberish", words: "Bzidk!!, aoiemm, boqler, panii, Aoogur, ABHP, PLEWNFO, Poubmu, Aiubg, POAAAA, muaopue, baogun, enoyeu, dsfi, oanve, obtmerm, dosueo, anr, adns")
-t6 = Theme.create(name: "star wars", words: "Force, Jedi, Troopers, lightsaber, Naboo, X-win, Leia, Skywalker, Chewbacca, R2-D2, Greedo, Darklighter, Stormtrooper, Kylo")
-t7 = Theme.create(name: "russian", words: "Dobraye, Spaseeba, Bal'shoye, Neechevo, Gavareetye, Pazhivayesh?, Gavaryoo, Prasteete, Neeploha!")
+t1 = Theme.create(name: "Star Wars", words: "Force, Jedi, Troopers, lightsaber, Naboo, X-win, Leia, Skywalker, Chewbacca, R2-D2, Greedo, Darklighter, Stormtrooper, Kylo, Force Awakens, Last Jedi, Qui-gon, Empire, Republic, Mos Eisley, Space Port, Corellian, Spaceship, Father, New Hope, Lucas, Saga, Clones, Droids, Galaxies, Resistance, Snoke, Lando")
+t2 = Theme.create(name: "Runtime Terror", words: "Blake, Caleb, Cornelius, Georgii, Gian, Placido, Raul, Jeannie, Somaia, Stephen, Steven, Vidhi, Mitchell, Paul, Vien, Kahoot, Lectures, Whiteboard, Ego-less, Teamwork, Code Challenge, Pair Programming, Discussions, Labs, learn.co, Flatiron, WeWork, 708 Main St, Houston, Jones Bldg")
+t3 = Theme.create(name: "The Office", words: "Bears, Beets, Chili, Jim, Dwight, Angela, Meredith, Stanley, Paper, Beesly, Halpert, Kevin, Andy, Creed, Kelly, Holly, Creed, Sales, Sabre, Pyramid, Scranton, Jello, Astrid, Chrysller, WUPHF.com, Dunder Mifflin, Parkour, Netflix, Miracle Legumes, Fire Drill")
+t4 = Theme.create(name: "Coding", words: "ActiveRecord, initialize, origin, attr_accessor, attr_reader, attr_writer, has_many, belongs_to, \#{speedy}, GitHub, C#, JavaScript, Ruby, mongoDB, SQLite3, binding.pry, Sinatra, React, Redux, NodeJS, AngularJS, end, elsif, API, Scraping, macOS, linux, windows, database, keyboard, mouse, SQL, mySQL, binary, AI")
+t5 = Theme.create(name: "Jibberish", words: "Bzidk!!, aoiemm, boqler, panii, Aoogur, ABHP, PLEWNFO, Poubmu, Aiubg, POAAAA, muaopue, baogun, enoyeu, dsfi, oanve, obtmerm, dosueo, anr, adns, PSFeh, diof, pehte, powow, dshfpw, PITE, AOHRBD")
+t6 = Theme.create(name: "Numbers", words: "34297, 0000000, 2134, 0793, 13946, 387, 12345, 60038, 110981, 20163, 73756, 657, 09821, 7490, 74940, 8754, 86459, 7599, 27037, 47475, 549327, 459245, 47507, 4711118, 84027, 76703, 12345, 98765") #Array.new(20){rand(1..100000)}
+t7 = Theme.create(name: "Russian", words: "Dobraye, Spaseeba, Bal'shoye, Neechevo, Gavareetye, Pazhivayesh?, Gavaryoo, Prasteete, Neeploha!, blyad, poluchish, paneemayoo, rooskee, Paka, Eezveeneete, srashnava")
 
 
 
@@ -53,16 +54,3 @@ Game.create(user_id: u4.id, theme_id: t4.id, score: 14)
 Game.create(user_id: u4.id, theme_id: t3.id, score: 18)
 Game.create(user_id: u4.id, theme_id: t2.id, score: 12)
 Game.create(user_id: u4.id, theme_id: t1.id, score: 5)
-
-
-
-
-
-
-
-
-
-
-
-
-
